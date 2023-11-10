b221b1e53eb8e21438b8a1e65c4d3fe8c9d42c26
Jam81202 <james.moore@g.fmarion.edu>
2023-11-10 13:58:13 -0500

    Huge Update to Mobile View
    Jordan Updated how the mobile view and also made changes to the HTML to help fix it.

--- LazyPepper Website/LazyPepper_Scripts.js
+++ LazyPepper Website/LazyPepper_Scripts.js
@@ -7,6 +7,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const customButton = document.getElementById("customButton");
     const contactButton = document.getElementById("contactButton");
     const galleryButton = document.getElementById("galleryButton");
+    const headerLogo = document.getElementById("header-logo");
 
     const hamburgerButton = document.getElementById("hamburger-button");
     const menu = document.getElementById("menu");
@@ -22,33 +23,37 @@ document.addEventListener("DOMContentLoaded", function () {
 
     hamburgerButton.addEventListener("click", function () {
         if (window.innerWidth <= 760) {
-            if (menu.style.display === "block") {
+            if ($("#menu").css("display") === "block") {
                 menu.style.display = "none";
+                headerLogo.style.display = "none";
                 fullMenu.style.top = "0px";
                 fullMenu.style.width = "40px"; // "25px" "2%"
                 fullMenu.style.height = "25px";
-                mainContainer.style.paddingLeft = "40px"; // "40px" "4%"
+                //mainContainer.style.paddingLeft = "40px"; // "40px" "4%"
                 mainContainer.style.marginTop = "5px";
 
             } else {
                 menu.style.display = "block";
+                headerLogo.style.display = "block";
                 fullMenu.style.top = ogMenuLoc;
                 fullMenu.style.width = ogMenuWidth;
                 fullMenu.style.height = ogMenuHeight;
-                mainContainer.style.paddingLeft = ogMainContainerPadding;
+                //mainContainer.style.paddingLeft = ogMainContainerPadding;
                 mainContainer.style.marginTop = ogMainContainerMarginTop;
             }
 
             // For Desktop displays
         } else {
-            if (menu.style.display === "block") {
+            if ($("#menu").css("display") === "block") {
                 menu.style.display = "none";
+                headerLogo.style.display = "none";
                 fullMenu.style.top = 0;
                 fullMenu.style.width = "25px"; // "25px" "2%"
                 mainContainer.style.paddingLeft = "40px"; // "40px" "4%"
 
             } else {
                 menu.style.display = "block";
+                headerLogo.style.display = "block";
                 fullMenu.style.top = ogMenuLoc;
                 fullMenu.style.width = ogMenuWidth;
                 mainContainer.style.paddingLeft = ogMainContainerPadding;
